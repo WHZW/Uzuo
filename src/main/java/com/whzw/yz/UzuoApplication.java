0a0ddf213dbc74e10ae2a602364dfb05a515ac7c
WuBoNing <411670513@qq.com>
2019-08-26 14:40:03 +0800

    完成记录超时功能，完善预定记录和预定日志，添加座位超时异常

--- src/main/java/com/whzw/yz/UzuoApplication.java
+++ src/main/java/com/whzw/yz/UzuoApplication.java
@@ -3,11 +3,13 @@ package com.whzw.yz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.whzw.yz.mapper")
 @EnableTransactionManagement
+@EnableScheduling
 public class UzuoApplication {
 
 	public static void main(String[] args) {
