3e85da5b060570cc1973ec0d21dc3e67187ac33a
WuBoNing <411670513@qq.com>
2019-08-29 10:53:43 +0800

    修改时间借口，返回今明后3天日期

---
+++ src/main/java/com/whzw/yz/vo/TimeVo.java
@@ -0,0 +1,42 @@
+package com.whzw.yz.vo;
+
+public class TimeVo {
+
+	private int year;
+	private int month;
+	private int day;
+	private int hour;
+	private char timeQuantum;
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	public char getTimeQuantum() {
+		return timeQuantum;
+	}
+	public void setTimeQuantum(char timeQuantum) {
+		this.timeQuantum = timeQuantum;
+	}
+	
+}
+
